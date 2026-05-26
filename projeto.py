c5fc9f83e6a7cb1b1123a08e0951ccfc2af23c09
dodo <dorgival2526@gmail.com>
2026-05-26 11:59:26 -0300

    Refactor input validation for user information

--- projeto.py
+++ projeto.py
@@ -1,5 +1,10 @@
 print('Bem vindo a empresa Nexct! A seguir preencha as informações para concorrer a vaga, Boa sorte')
-nome = input('Digite seu nome completo sem abreviações: ')
+while True:
+    nome = input('Digite seu nome completo sem abreviações: ')
+    if nome.isalpha():
+        break
+    else:
+        print('Tente novamente, apenas letras')
 while True:
     idade = str(input('Digite sua idade somente números: '))
     if idade.isdigit():
@@ -12,9 +17,23 @@ while True:
         break
     else:
         print('Tente novamente, apenas números.')
+while True:        
+    dif = input('Qual seu diferencial que agregaria na vaga que está concorrendo?: ')
+    if dif.isalpha():
+        break
+    else:
+        print('Tente novamente, apenas letras')
+while True:        
+    motivo = input('Porque deveriamos te contratar?: ')
+    if motivo.isalpha():
+        break
+    else:
+        print('Tente novamente, apenas letras')
+while True:        
+    experiencia = input('Fale breviamente sobre suas experiências profissionais ao longo de sua carreira no mercado de trabalho: ')
+    if experiencia.isalpha():
+        break
+    else:
+        print('Tente novamente, apenas letras')
         
-dif = input('Qual seu diferencial que agregaria na vaga que está concorrendo?: ')
-motivo = input('Porque deveriamos te contratar?: ')
-experiencia = input('Fale breviamente sobre suas experiências profissionais ao longo de sua carreira no mercado de trabalho: ')
-
 print('Obrigado por preencher as informações, aguarde o retorno da empresa via email! ')
