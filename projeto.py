bd54882dbd6b1588b5916acd624dc73dca8f4ee7
dodo <dorgival2526@gmail.com>
2026-07-07 09:57:33 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -1,7 +1,13 @@
+def validar_nome(nome):
+    # Remove os espaços e checa se o que sobrou são apenas letras
+    if nome.replace(" ", "").isalpha():
+        return True
+    return False 
 print('Bem vindo a empresa Nexct! A seguir preencha as informações para concorrer a vaga, Boa sorte')
 while True:
     nome = input('Digite seu nome completo sem abreviações: ')
-    if nome.isalpha():
+    if validar_nome(nome):
+        print("Nome válido e cadastrado com sucesso!")
         break
     else:
         print('Tente novamente, apenas letras')
@@ -14,24 +20,31 @@ while True:
 while True:
     cpf = str(input('Digite seu CPF completo, somente números: '))
     if cpf.isdigit():
-        break
+        if len(cpf)!=11:
+            print('Quantidade de números invalidos, tente novamente. ')
+        else:
+            print('CPF valido! ')        
+            break
     else:
         print('Tente novamente, apenas números.')
 while True:        
     dif = input('Qual seu diferencial que agregaria na vaga que está concorrendo?: ')
-    if dif.isalpha():
+    if validar_nome(nome):
+        print("Nome válido e cadastrado com sucesso!")
         break
     else:
         print('Tente novamente, apenas letras')
 while True:        
     motivo = input('Porque deveriamos te contratar?: ')
-    if motivo.isalpha():
+    if validar_nome(nome):
+        print("Nome válido e cadastrado com sucesso!")
         break
     else:
         print('Tente novamente, apenas letras')
 while True:        
     experiencia = input('Fale breviamente sobre suas experiências profissionais ao longo de sua carreira no mercado de trabalho: ')
-    if experiencia.isalpha():
+    if validar_nome(nome):
+        print("Nome válido e cadastrado com sucesso!")
         break
     else:
         print('Tente novamente, apenas letras')
